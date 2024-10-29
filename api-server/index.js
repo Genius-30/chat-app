e07f10bd9e1cb36c71d1b9cddbd2a72f64675d2e
Genius30 <geniusporwal01@gmail.com>
2024-10-29 16:59:31 +0530

    Merge main and socket server files in one

--- api-server/index.js
+++ api-server/index.js
@@ -6,30 +6,60 @@ import { userRouter } from "./routes/user.route.js";
 import { chatRouter } from "./routes/chat.route.js";
 import cookieParser from "cookie-parser";
 import http from "http";
-import {
-  getSocketInstance,
-  initializeSocket,
-} from "../socket-server/server.js";
+import { Server } from "socket.io";
+
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-initializeSocket(server);
 
+// Initialize Socket.IO with CORS
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+  },
+});
+
+// Socket.IO event handling
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("joinRoom", (chatId) => {
+    socket.join(chatId);
+    console.log(`User ${socket.id} joined room ${chatId}`);
+  });
+
+  socket.on("sendMessage", (message) => {
+    // Broadcast the message to other users in the room
+    socket.broadcast.to(message.chatId).emit("message", message);
+  });
+
+  socket.on("leaveRoom", (chatId) => {
+    socket.leave(chatId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// Middleware to add io instance to every request
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+// Express middleware setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
-// Middleware to add io instance to every request
-app.use((req, res, next) => {
-  req.io = getSocketInstance();
-  next();
-});
-
 // Connect to MongoDB
 dbConnect();
 
+// API Routes
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
@@ -37,6 +67,7 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRouter);
 app.use("/api/chat", chatRouter);
 
+// Start the server
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
   console.log(`API Server running on port ${PORT}`);
